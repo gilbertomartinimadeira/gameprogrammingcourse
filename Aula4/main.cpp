d21926899464d3ec2ce5b51ab6f45e32a7d2e86b
Gilberto Martini <gilberto.madeira@outlook.com>
2022-01-22 15:30:16 -0300

    1 tentativa de gerar uma tela com um circulo verde

--- Aula4/main.cpp
+++ Aula4/main.cpp
@@ -10,8 +10,45 @@
  */
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char * argv[])
 {
-    std::cout << "Teste pra ver se funciona" << '\n';
+    const int wWidth = 800;
+    const int wHeight = 600;
+
+    sf::RenderWindow window(sf::VideoMode(wWidth,wHeight), "SFML Funciona!");
+
+    sf::CircleShape circle(50);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition(wWidth/2, wHeight/2);
+    float circleMoveSpeed  = 0.02;
+
+    sf::Font myFont;
+
+    if(!myFont.loadFromFile("fonts/arial.ttf"))
+    {
+        std::cerr << "Could not load font from file \n";
+    }
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+
+
+
+
+
+
+
 }
