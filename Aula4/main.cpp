7eb78c9826bc58c5a7ef2c2686df3e3ecddf90e0
Gilberto Martini <gilberto.madeira@outlook.com>
2022-01-22 14:13:24 -0300

    adiciona os arquivos da atribuiçao 1

---
+++ Aula4/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Gilberto Martini Madeira <gilmartmd@gmail.com>
+ * @brief as a sign of appreciation, I'm quoting Dave Churchill, who is an excellent instructor, hope someday he'll read it!
+ * @version 0.1
+ * @date 2022-01-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+    std::cout << "Teste pra ver se funciona" << '\n';
+}
