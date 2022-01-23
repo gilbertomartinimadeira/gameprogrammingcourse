0465d7b1346c328a56f44b90f8b8b88e147fd455
Gilberto Martini <gilberto.madeira@outlook.com>
2022-01-22 23:33:13 -0300

    imprime os limites locais do circulo

--- Aula4/src/main.cpp
+++ Aula4/src/main.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <fstream>
 
+
 int main(int argc, char * argv[])
 {
     const int wWidth = 800;
@@ -19,10 +20,10 @@ int main(int argc, char * argv[])
 
     sf::RenderWindow window(sf::VideoMode(wWidth,wHeight), "SFML Funciona!");
 
-    sf::CircleShape circle(50);
+    sf::CircleShape circle(60);
     circle.setFillColor(sf::Color::Green);
     circle.setPosition(wWidth/2, wHeight/2);
-    float circleMoveSpeed  = -0.02f;
+    float circleMoveSpeed  = -0.04f;
 
     sf::Font myFont;
 
@@ -32,20 +33,34 @@ int main(int argc, char * argv[])
         exit(-1);
     }
 
-    sf::Text myText("Eu Aqui seguindo o curso de programacao de jogos com c++ e SFML",myFont,18);
+    
 
-    myText.setPosition(0, wHeight - (float)myText.getCharacterSize() - 2) ;
+    
+    
 
     int counter = 0;
 
     while(window.isOpen())
-    {
-        sf::Vector2f previousCirclePosition = circle.getPosition(); 
-        sf::Vector2f movement(circleMoveSpeed,circleMoveSpeed);
-        sf::Vector2f newPosition = previousCirclePosition + movement;
+    {          
+        auto circlePositionTextString = "Circle Position: (" + std::to_string(circle.getPosition().x) +","+ std::to_string(circle.getPosition().y) + ")";      
+        sf::Text circlePositionText(circlePositionTextString, myFont,16);
+        circlePositionText.setPosition(10, wHeight - (float)circlePositionText.getCharacterSize() - 2) ;
+
+        auto circleLocalBounds = circle.getLocalBounds();
+        
+        
+        auto circleBoundingBoxTextString = " Circle Local Bounds \n width: " + std::to_string(circleLocalBounds.width)
+                                        +  " \n height: " + std::to_string(circleLocalBounds.height)
+                                        +  " \n top: " + std::to_string(circleLocalBounds.top) 
+                                        +  " \n left: " + std::to_string(circleLocalBounds.left);
+                                          
+        sf::Text circleBoundingBoxText(circleBoundingBoxTextString,myFont,16);
+        circleBoundingBoxText.setPosition(circlePositionText.getPosition().x + 10, (float)circleBoundingBoxText.getCharacterSize() - 2);
 
         sf::Event event;
-        
+
+        circlePositionText.setString(circlePositionTextString);
+        circleBoundingBoxText.setString(circleBoundingBoxTextString);
 
         while (window.pollEvent(event))
         {
@@ -53,25 +68,31 @@ int main(int argc, char * argv[])
             if(event.type == sf::Event::KeyPressed)
             {
                 std::cout << "Key pressed! " << event.key.code << "\n";
-                if(event.key.code == 23)
-                {
+                if(event.key.code == sf::Keyboard::X)
                     circle.setFillColor(sf::Color::Red);                
-                }
+                
             }    
             if(event.type == sf::Event::MouseButtonPressed)
             {
                 std::cout << "Mouse pressed! " << event.mouseButton.button << "\n";            
-                if(event.mouseButton.button == 0)
-                circle.setFillColor(sf::Color::Yellow);                
+                switch(event.mouseButton.button){
+                    case 0: circle.setFillColor(sf::Color::Yellow);  
+                    break;        
+                    case 1: circleMoveSpeed *= -1;      
+                    break;                
+                }
             }
         }
         
-
+        sf::Vector2f previousCirclePosition = circle.getPosition(); 
+        sf::Vector2f movement(circleMoveSpeed, circleMoveSpeed);
+        sf::Vector2f newPosition = previousCirclePosition + movement;
         circle.setPosition(newPosition);
     
-        window.clear(sf::Color::White);
+        window.clear();
         window.draw(circle);
-        window.draw(myText);
+        window.draw(circlePositionText);
+        window.draw(circleBoundingBoxText);
         window.display();
     }
 
