17b1341b893471b04fa45a6888360c3ba2c9c8a9
Gilberto Martini <gilberto.madeira@outlook.com>
2022-01-30 19:39:10 -0300

    lendo os componentes pra dentro do sistema

--- Aula4/assignment/main.cpp
+++ Aula4/assignment/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <SFML/Graphics.hpp>
@@ -15,13 +16,30 @@ protected:
     
     int m_width;
     int m_height;    
-    Shape(int width, int height):m_width(width), m_height(height){}
+    std::string m_name;
+    std::string m_type;
+
+    Shape(int width, 
+          int height, 
+          const std::string name, 
+          const std::string type):
+                                  m_width(width), 
+                                  m_height(height),
+                                  m_name(name),
+                                  m_type(type){}
+    
 
 public:
-    virtual int getArea() = 0;
+    //virtual int getArea() = 0;
+    const std::string toString()
+    {
+        return "\ntype: " + m_type + 
+                "\nname: " + m_name + 
+                "\nwidth:" + std::to_string(m_width) + 
+                "\nheight: " + std::to_string(m_height) + "\n\n";
+    }
 
 };
-
 // Derived classes
 class Rectangle: public Shape 
 {
@@ -30,16 +48,19 @@ public:
     int getArea() { 
         return (m_width * m_height); 
     }
-    Rectangle(int width, int height): Shape { width, height }{};
+    Rectangle(int width, 
+              int height, 
+              const std::string name, 
+              const std::string type): Shape { width, height, name, type}{};
 };
 
 class Triangle: public Shape 
 {
-   public:
-      int getArea() { 
-         return (m_width * m_height)/2; 
-      }
-    Triangle(int width, int height): Shape { width, height }{};
+//    public:
+//       int getArea() { 
+//          return (m_width * m_height)/2; 
+//       }
+    //Triangle(int width, int height): Shape { width, height }{};
 };
 
 class Circle : public Shape 
@@ -47,51 +68,93 @@ class Circle : public Shape
     private:
     int m_radius;
 
-    public:
-    int getArea()
-    {
-        return M_PI * pow(m_radius,2);
-    }
+    // public:
+    // int getArea()
+    // {
+    //     return M_PI * pow(m_radius,2);
+    // }
 
-    Circle(int radius): m_radius(radius), Shape { m_width  = 0, m_height = 0}{};
+    //Circle(int radius): m_radius(radius), Shape { m_width  = 0, m_height = 0}{};
     
 };
-
 
 
 class ConfigManager
 {
-    private:
-    
-
-    //todo: read config file into shape objects
     public:
-    std::vector<Shape> loadShapesFromConfigFile(const std::string & fileName)
-    {
-        
+    static std::vector<Shape> loadShapesFromConfigFile(const std::string & fileName)
+    {        
         std::ifstream inputFileStream(fileName);
         
-        std::string type, name ,position, speed,  width, height, radius, red, green, blue;
+        std::string type, name ,
+                    xPositionString,
+                    yPositionString,
+                    xSpeedString,
+                    ySpeedString,
+                    widthString,
+                    heightString,
+                    radiusString,
+                    redString,
+                    greenString,
+                    blueString;
+
+        int xPosition, yPosition, xSpeed, ySpeed, width, height, radius, red, green, blue;
+
 
         std::vector<Shape> shapes;
 
         
-    
         while(inputFileStream >> type)
-        {
-            if(type == "Window"){ }
-            else if( type == "Font") {}
-            else if( type == "Rectangle") {}
-            else if( type == "Circle") {}
-            else if( type == "Triangle") {}
+        {        
+            if( type == "Rectangle") 
+            {
+                inputFileStream >> name >> 
+                                   xPositionString >> 
+                                   yPositionString >> 
+                                   xSpeedString >> 
+                                   ySpeedString >> 
+                                   widthString >> 
+                                   heightString >> 
+                                   redString >> 
+                                   greenString >> 
+                                   blueString;            
+
+                width = std::stoi(widthString); 
+                height = std::stoi(heightString);
+
+
+                shapes.push_back(Rectangle { width, height, name, type });
+            }
+
+            else if( type == "Circle") {
+
+            }
             
         }
-    
+        return shapes;
     }
 
 };
 
+int setupWindow()
+{
+    auto type = "Window";
+    
+    if(type == "Window"){ }
+            else if( type == "Font") {}
+
+            return 0;
+}
+
 int main(int argc, char * argv[])
 {
+
+    std::vector<Shape> shapes = ConfigManager::loadShapesFromConfigFile("config.txt");
+    
+    for(int i = 0; i < shapes.size(); i++)
+    {
+        std::cout << shapes[i].toString();
+    }
+    
     return 0;
 }
