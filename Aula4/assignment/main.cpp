8bc6405725bd0a98ff6b2da925e73fd0138895da
Gilberto Martini <gilberto.madeira@outlook.com>
2022-01-30 22:51:55 -0300

    inclui suporte a leitura de circulos

--- Aula4/assignment/main.cpp
+++ Aula4/assignment/main.cpp
@@ -8,6 +8,24 @@
 #define _USE_MATH_DEFINES
 
 
+class ShapeDTO {
+public:
+    std::string m_type,
+                m_name;
+
+    int m_xPosition,
+        m_yPosition,    
+        m_red, 
+        m_green,
+        m_blue,
+        m_width,
+        m_height,
+        m_radius;
+
+    float m_xSpeed,
+          m_ySpeed;
+};
+
 class Shape
 {
 protected:
@@ -46,28 +64,9 @@ protected:
             m_blue(blue) {}
 
 public:
-    //virtual int getArea() = 0;
-
-    virtual const std::string toString()
-    {
-        return "\ntype: " + m_type + 
-                "\nname: " + m_name +                     
-                "\nX Position: " + std::to_string(m_xPosition) + 
-                "\nY Position: " + std::to_string(m_yPosition) +
-                "\nX Speed: " + std::to_string(m_xSpeed) + 
-                "\nY Speed: " + std::to_string(m_ySpeed) +
-                "\nRed: " + std::to_string(m_red) +
-                "\nGreen: " + std::to_string(m_green) +
-                "\nBlue: " + std::to_string(m_blue);
-                
-                //TODO: Adicionar ao toString do retangulo
-
-                // "\nwidth: " + std::to_string(m_width) + 
-                // "\nheight: " + std::to_string(m_height) +
-    }
-
+    virtual int getArea() = 0;
 };
-// Derived classes
+
 class Rectangle: public Shape 
 {
 private:
@@ -99,32 +98,78 @@ public:
                                       blue },
                                       m_width(width),
                                       m_height(height){}
-             
-              
-};
 
+    const std::string toString() 
+    {
+        return "\nType: " + m_type + 
+               "\nName: " + m_name +                     
+               "\nX Position: " + std::to_string(m_xPosition) + 
+               "\nY Position: " + std::to_string(m_yPosition) +
+               "\nX Speed: " + std::to_string(m_xSpeed) + 
+               "\nY Speed: " + std::to_string(m_ySpeed) +
+               "\nRed: " + std::to_string(m_red) +
+               "\nGreen: " + std::to_string(m_green) +
+               "\nBlue: " + std::to_string(m_blue) +        
+               "\nWidth: " + std::to_string(m_width) + 
+               "\nHeight: " + std::to_string(m_height);
+    }
+                         
+};
 
 class Circle : public Shape 
 {
-    private:
+private:
     int m_radius;
 
-    // public:
-    // int getArea()
-    // {
-    //     return M_PI * pow(m_radius,2);
-    // }
-
-    //Circle(int radius): m_radius(radius), Shape { m_width  = 0, m_height = 0}{};
+    public:
+    int getArea()
+    {
+        return M_PI * pow(m_radius,2);
+    }
+public:
+    Circle(
+        const std::string type,
+        const std::string name,             
+        int xPosition,
+        int yPosition,
+        float xSpeed,
+        float ySpeed,
+        int red,
+        int green,
+        int blue,
+        int radius) : Shape { type, 
+                              name,
+                              xPosition,
+                              yPosition,
+                              xSpeed,
+                              ySpeed,
+                              red,
+                              green,
+                              blue },
+                              m_radius(radius){}
+                              const std::string toString() 
+    {
+        return "\nType: " + m_type + 
+               "\nName: " + m_name +                     
+               "\nX Position: " + std::to_string(m_xPosition) + 
+               "\nY Position: " + std::to_string(m_yPosition) +
+               "\nX Speed: " + std::to_string(m_xSpeed) + 
+               "\nY Speed: " + std::to_string(m_ySpeed) +
+               "\nRed: " + std::to_string(m_red) +
+               "\nGreen: " + std::to_string(m_green) +
+               "\nBlue: " + std::to_string(m_blue) +        
+               "\nRadius: " + std::to_string(m_radius);
+    }
     
 };
-
 
 class ConfigManager
 {
     public:
-    static std::vector<Shape> loadShapesFromConfigFile(const std::string & fileName)
+    static std::vector<ShapeDTO> loadConfiguration(const std::string & fileName)
     {        
+        std::vector<ShapeDTO> shapeDTOs;
+
         std::ifstream inputFileStream(fileName);
         
         std::string type, 
@@ -152,54 +197,66 @@ class ConfigManager
         float xSpeed,
               ySpeed;
 
-        std::vector<Shape> shapes;
-
-        
         while(inputFileStream >> type)
         {        
+            inputFileStream >> name >> 
+                                xPositionString >> 
+                                yPositionString >> 
+                                xSpeedString >> 
+                                ySpeedString >>                                 
+                                redString >> 
+                                greenString >> 
+                                blueString;
+                                   
             if( type == "Rectangle") 
             {
-                inputFileStream >> name >> 
-                                   xPositionString >> 
-                                   yPositionString >> 
-                                   xSpeedString >> 
-                                   ySpeedString >>                                 
-                                   redString >> 
-                                   greenString >> 
-                                   blueString >>
-                                   widthString >> 
-                                   heightString;            
-            
-                xPosition = std::stoi(xPositionString);
-                yPosition = std::stoi(yPositionString);
-                xSpeed = std::stof(xSpeedString);
-                ySpeed = std::stof(xSpeedString);
-                red = std::stoi(redString);
-                green = std::stoi(greenString);
-                blue = std::stoi(blueString);
+                inputFileStream >> widthString >> 
+                                   heightString;    
+
+                
                 width = std::stoi(widthString); 
-                height = std::stoi(heightString);
-
-
-                shapes.push_back(Rectangle {type, 
-                                            name, 
-                                            xPosition, 
-                                            yPosition,
-                                            xSpeed, 
-                                            ySpeed,
-                                            red, 
-                                            green, 
-                                            blue, 
-                                            width, 
-                                            height });
+                height = std::stoi(heightString);            
             }
 
             else if( type == "Circle") {
+                inputFileStream >> radiusString;
+                radius = std::stoi(radiusString);
+            }   
 
+            else continue;
+
+            try
+            {
+                xPosition = std::stoi(xPositionString);
+                yPosition = std::stoi(yPositionString);
+                xSpeed = std::stof(xSpeedString);
+                ySpeed = std::stof(ySpeedString);
+                red = std::stoi(redString);
+                green = std::stoi(greenString);
+                blue = std::stoi(blueString);                                
             }
-            
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+                        
+            ShapeDTO dto;
+            dto.m_type = type;
+            dto.m_name = name;
+            dto.m_xPosition = xPosition;
+            dto.m_yPosition = yPosition;
+            dto.m_xSpeed = xSpeed;
+            dto.m_xSpeed = ySpeed;
+            dto.m_red = red;
+            dto.m_green = green;
+            dto.m_blue = blue;
+            dto.m_width = width;
+            dto.m_height = height;
+            dto.m_radius = radius;
+
+            shapeDTOs.push_back(dto);        
         }
-        return shapes;
+        return shapeDTOs;
     }
 
 };
@@ -216,10 +273,55 @@ int setupWindow()
 
 int main(int argc, char * argv[])
 {
+    std::vector<ShapeDTO> dtos = ConfigManager::loadConfiguration("config.txt");
 
-    std::vector<Shape> shapes = ConfigManager::loadShapesFromConfigFile("config.txt");
-    
-    for(int i = 0; i < shapes.size(); i++) std::cout << shapes[i].toString() << "\n";
-    
+    std::vector<Rectangle> rectangles;
+    std::vector<Circle> circles;
+
+    for (auto dto : dtos)
+    {
+        if(dto.m_type == "Rectangle")
+        {
+            std::cout << "Rectangle found, adding to vector...\n";
+            rectangles.push_back(Rectangle( 
+                                            dto.m_type, 
+                                            dto.m_name,
+                                            dto.m_xPosition,
+                                            dto.m_yPosition,
+                                            dto.m_xSpeed,
+                                            dto.m_ySpeed,
+                                            dto.m_red,
+                                            dto.m_green,
+                                            dto.m_blue,
+                                            dto.m_width,
+                                            dto.m_height));
+        } 
+        else if(dto.m_type == "Circle") 
+        {
+            std::cout << "Circle found! adding to vector...\n";
+            circles.push_back(Circle( 
+                                    dto.m_type, 
+                                    dto.m_name,
+                                    dto.m_xPosition,
+                                    dto.m_yPosition,
+                                    dto.m_xSpeed,
+                                    dto.m_ySpeed,
+                                    dto.m_red,
+                                    dto.m_green,
+                                    dto.m_blue,
+                                    dto.m_radius));
+        }
+        
+    }
+
+    for(auto c : circles)
+    {
+        std::cout << c.toString() << "\n";
+    }
+    for(auto r : rectangles)
+    {
+        std::cout << r.toString() << "\n";
+    }
+        
     return 0;
 }
